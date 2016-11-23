96322f609c28a096805463bb570ca3ab90e131e6
Nikola Stojiljkovic <nikolavla@gmail.com>
2016-11-23 23:17:06 +0000

    More tests

--- src/test/scala/freezing/common/scala/ErrorsTest.scala
+++ src/test/scala/freezing/common/scala/ErrorsTest.scala
@@ -1,6 +1,7 @@
 package freezing.common.scala
 
 import freezing.common.scala.errors._
+import freezing.common.scala.results.{Bad, EResult}
 import org.scalatest.FlatSpec
 
 class ErrorsTest extends FlatSpec {
@@ -11,6 +12,20 @@ class ErrorsTest extends FlatSpec {
   }
   it should "create Errors case class from a non-empty list" in {
     val nel = List(Error.fromThrowable(new Exception("This can be any error")))
+    Errors(nel)
     assert(nel.size === 1)
+  }
+  it should "have two errors for the list of two Errors and one Success" in {
+    val nel = List(
+      EResult(1),
+      EResult(Error.fromThrowable(new Exception("This can be any error"))),
+      EResult(3),
+      EResult(Error.fromThrowable(new Exception("Another error")))
+    )
+
+    assert(nel.validate.isBad)
+    nel.validate match {
+      case Bad(errors) => assert(errors.asInstanceOf[Errors].nel.size === 2)
+    }
   }
 }
